47c194db48e22e1e51a838ea2587126953573128
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2022-01-29 00:49:21 +0530

    hospital/ post method is ready

--- hospital/views.py
+++ hospital/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-
+@csrf_exempt
 def pathHospital(request):
     if(request.method == 'GET'):
         return listHospitals(request)
@@ -19,28 +20,40 @@ def listHospitals(request):
     # data = list(items)
     # response = json.dumps(data)
     response = Hosp.getHospitalsAsPerQuery(request)
-    return HttpResponse(response, content_type="application/json", status=203)
+    return HttpResponse(response, content_type="application/json", status=201)
 
 
-# This is for the post method in /hospital and it is yet to be corrected.
+
+@csrf_exempt
 def addHospital(request):
-    name = request.POST.get('name', '')
-    speciality = request.POST.get('speciality', '')
-    costWard = request.POST.get('costWard', '')
-    rating = request.POST.get('rating', '')
-    typeGP = request.POST.get('typeGP', '')
-    contact = request.POST.get('contact', '')
-    covid = request.POST.get('covid', '')
-    army = request.POST.get('army', '')
-    availableBeds = request.POST.get('availableBeds', '')
-    state = request.POST.get('state', '')
-    district = request.POST.get('district', '')
-    pincode = request.POST.get('pincode', '')
-    timings = request.POST.get('timings', '')
-    Hospital = Hosp(name=name,speciality=speciality,costWard=costWard,rating =rating , typeGP=typeGP, contact=contact,covid=covid,army=army,availableBeds=availableBeds,state=state,district=district,pincode=pincode,timings=timings)
+    # print("..................the post request is reaching here...............")
+    # print(request.body)
+    # print("......................................................")
+    # print(json.loads(request.body))
+    # print("......................................................")
+    # print(type(json.loads(request.body)))
+    # print(".......................................................")
+
+    # name = request.POST.get('name', '')
+    # speciality = request.POST.get('speciality', '')
+    # costWard = request.POST.get('costWard', '')
+    # rating = request.POST.get('rating', '')
+    # typeGP = request.POST.get('typeGP', '')
+    # contact = request.POST.get('contact', '')
+    # covid = request.POST.get('covid', '')
+    # army = request.POST.get('army', '')
+    # availableBeds = request.POST.get('availableBeds', '')
+    # state = request.POST.get('state', '')
+    # district = request.POST.get('district', '')
+    # pincode = request.POST.get('pincode', '')
+    # timings = request.POST.get('timings', '')
     
+    received_json_data=json.loads(request.body)
+    # Hospital = Hosp(name=name,speciality=speciality,costWard=costWard,rating =rating , typeGP=typeGP, contact=contact,covid=covid,army=army,availableBeds=availableBeds,state=state,district=district,pincode=pincode,timings=timings)
+    Hospital = Hosp(**received_json_data)
     Hospital.save()
-    return HttpResponse([{"hello":"world"}])
+    response = json.dumps(received_json_data)
+    return HttpResponse(response, content_type="application/json", status=201)
 
 # This is not yet finalised 
 def getHospitalbyid(request,myid):
